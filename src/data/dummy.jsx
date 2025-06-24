702c24537c6464afc31c3e89a8b493bb6c5c5a6b
GunaSundari <166094238+RGunaSundari@users.noreply.github.com>
2025-06-24 09:50:45 +0530

    Initial commit

--- src/data/dummy.jsx
+++ src/data/dummy.jsx
@@ -1,10 +1,10 @@
 
 export default{
-    firstName:'James',
-    lastName:'Carter',
+    firstName:'Guna',
+    lastName:'Sundari',
     jobTitle:'full stack developer',
     address:'525 N tryon Street, NC 28117',
-    phone:'(123)-456-7890',
+    phone:'+91 0486874654',
     email:'exmaple@gmail.com',
     themeColor:"#ff6666",
     summery:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -13,9 +13,9 @@ export default{
             id:1,
             title:'Full Stack Developer',
             companyName:'Amazon',
-            city:'New York',
+            city:'Chennai',
             state:'NY',
-            startDate:'Jan 2021',
+            startDate:'Jan 2022',
             endDate:'',
             currentlyWorking:true,
             workSummery:'• Designed, developed, and maintained full-stack applications using React and Node.js.\n'+
@@ -45,18 +45,18 @@ export default{
     education:[
         {
             id:1,
-            universityName:'Malwa institue indore',
-            startDate:'Aug 2018',
-            endDate:'Dec:2019',
-            degree:'B.tech',
+            universityName:'University college of engineering villupuram',
+            startDate:'Aug 2022',
+            endDate:'Dec:2026',
+            degree:'B.E',
             major:'Computer Science',
             description:'Lorem ipsum dolor sit amet, consectetur adipiscing '
         },
         {
             id:2,
-            universityName:'Model H.S. school',
-            startDate:'Aug 2018',
-            endDate:'Dec:2019',
+            universityName:'John dewey H.S. school',
+            startDate:'Aug 2020',
+            endDate:'Dec:2022',
             degree:'Higher Secoundry',
             major:'PCM',
             description:'Lorem ipsum dolor sit amet, consectetur adipiscing '
@@ -91,7 +91,7 @@ export default{
           description:'Lorem1 ipsum dolor sit ametlkvklnsdklv sdmvvknkso smdkpvmvkonknnffkondsoinuo mlcnajhbuoivjwbd  lkmaknjifdf',
           LanguagesUsed: 'Java, JS, React, Express, JWT',
           GitHub: "View Repository",
-          GitHubRepoLink: "https://github.com/ashutoshgothwal04/News-from-Api-server"
+          GitHubRepoLink: "https://github.com/rgunasundari/News-from-Api-server"
         },
         {
           id:2,
@@ -99,7 +99,7 @@ export default{
           description:'Lorem1 ipsum dolor sit ametlkvklnsdklv sdmvvknkso smdkpvmvkonknnffkondsoinuo mlcnajhbuoivjwbd  lkmaknjifdf',
           LanguagesUsed: 'Python, JS, React, Java, JWT',
           GitHub: "View Repository",
-          GitHubRepoLink: "https://github.com/ashutoshgothwal04/World-expo"
+          GitHubRepoLink: "https://github.com/rgunasundari/World-expo"
         },
         {
           id:3,
@@ -107,7 +107,7 @@ export default{
           description:'Lorem1 ipsum dolor sit ametlkvklnsdklv sdmvvknkso smdkpvmvkonknnffkondsoinuo mlcnajhbuoivjwbd  lkmaknjifdf',
           LanguagesUsed: 'OpenCV, React, Python, Next.js, JWT',
           GitHub: "View Repository",
-          GitHubRepoLink: "https://github.com/ashutoshgothwal04/Food-Menu"
+          GitHubRepoLink: "https://github.com/rgunasundari/Food-Menu"
         }
       ]
       
